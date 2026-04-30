0bf1269fb6a5b685a58c58dccd8a887b82b5cb94
shivuchatur445-bit <shivuchatur445@gmail.com>
2026-04-30 18:40:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Generator from "./Generator";
+import Generator from "./Generator.jsx"; // Humne .jsx laga diya taaki Vercel ko mil jaye
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Generator />} />
-        </Routes>
-      </BrowserRouter>
+      <Generator />
     </div>
   );
 }
